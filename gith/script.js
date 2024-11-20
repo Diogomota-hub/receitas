47f07ce8dc6dd3987b762025c972e85cb92d3643
Diogomota-hub <dyogomotta234@gmail.com>
2024-11-20 12:42:33 -0300

    Add files via upload
    site que fiz principalmente para treinar as animações básicas em css e colocar links .

---
+++ gith/script.js
@@ -0,0 +1,24 @@
+function salvarbolo(){
+    let fav= document.createElement('p')
+    fav.innerHTML= 'Bolo de cenoura'
+    document.getElementById('adicionar').appendChild(fav)
+    fav.style.color= 'white'
+    fav.style.margin= '10px'
+}
+
+function salvarfeijoada(){
+    let fav = document.createElement('p')
+    fav.innerHTML= 'Feijoada'
+    document.getElementById('adicionar').appendChild(fav)
+    fav.style.color= 'white'
+    fav.style.margin= '10px'
+}
+
+function salvarlasanha(){
+    let fav = document.createElement('p')
+    fav.innerHTML= 'Lasanha'
+    document.getElementById('adicionar').appendChild(fav)
+    fav.style.color= 'white'
+    fav.style.margin= '10px'
+}
+
